087c404ad7ce7cb6910a6bdce15f6eabc1a954ce
Filip Chalupa <chalupa.filip@gmail.com>
2023-02-02 23:46:05 +0100

    Get names

---
+++ scripts/content.js
@@ -0,0 +1,27 @@
+const debounce = (f, timeout = 300) => {
+	let timer
+	return (...args) => {
+		clearTimeout(timer)
+		timer = setTimeout(() => {
+			f.apply(this, args)
+		}, timeout)
+	}
+}
+
+const findNames = debounce(() => {
+	const names = Array.from(
+		document.querySelectorAll('.participants-item__display-name'),
+	).map((name) => name.textContent)
+	console.log(names)
+})
+
+const observer = new MutationObserver((mutationsList) => {
+	mutationsList.forEach((mutation) => {
+		mutation.addedNodes.forEach(() => {
+			findNames()
+		})
+	})
+})
+
+// @TODO: maybe use setInterval/Timeout too
+observer.observe(document, { subtree: true, childList: true })
