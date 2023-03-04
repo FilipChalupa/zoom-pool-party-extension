87aafd76c8738c29ebb19c96e1a7e456b1891ade
Filip Chalupa <chalupa.filip@gmail.com>
2023-03-05 00:53:28 +0100

    Communicate with firebase

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,59 @@
-chrome.runtime.onMessage.addListener((request) => {
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.1.0/firebase-app.js'
+import {
+	deleteDoc,
+	doc,
+	getFirestore,
+	setDoc,
+} from 'https://www.gstatic.com/firebasejs/9.1.0/firebase-firestore.js'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCk-rS-4QmPAXIBE2xIbPBYDzxjqp5Gc1A',
+	authDomain: 'zoom-pool-party.firebaseapp.com',
+	projectId: 'zoom-pool-party',
+	storageBucket: 'zoom-pool-party.appspot.com',
+	messagingSenderId: '225593866133',
+	appId: '1:225593866133:web:f1c463dfa3c526f762689c',
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+console.log(db)
+
+const poolId = 'RGGAKI4GcohaIXbaSs3v' // @TODO: get from popup
+//const participantsRef = collection(db, "pools", poolId, "participants");
+
+chrome.runtime.onMessage.addListener(async (request) => {
 	if ('participantsCount' in request) {
 		chrome.action.setBadgeText({
 			text: request.participantsCount.toString(),
 		})
+	}
+	if ('reset' in request && request.reset) {
+		console.log('Reset')
+		const docRef = doc(db, 'pools', poolId)
+		await deleteDoc(docRef)
+	}
+	if ('addParticipants' in request && request.addParticipants.length > 0) {
+		console.log('Add participants')
+		console.log(request.addParticipants)
+		// @TODO: batching
+		for (const participant of request.addParticipants) {
+			const docRef = doc(db, 'pools', poolId, 'participants', participant.id)
+			await setDoc(docRef, {
+				name: participant.name,
+			})
+		}
+	}
+	if (
+		'removeParticipants' in request &&
+		request.removeParticipants.length > 0
+	) {
+		console.log('Remove participants')
+		console.log(request.removeParticipants)
+		// @TODO: batching
+		for (const participant of request.addParticipants) {
+			const docRef = doc(db, 'pools', poolId, 'participants', participant.id)
+			await deleteDoc(docRef)
+		}
 	}
 })
--- scripts/content.js
+++ scripts/content.js
@@ -1,28 +1,70 @@
-const debounce = (f, timeout = 300) => {
-	let timer
-	return (...args) => {
-		clearTimeout(timer)
-		timer = setTimeout(() => {
-			f.apply(this, args)
-		}, timeout)
+console.log('Pool party extension injected')
+;(async () => {
+	const hash = (s) =>
+		s
+			.split('')
+			.reduce((a, b) => {
+				a = (a << 5) - a + b.charCodeAt(0)
+				return a & a
+			}, 0)
+			.toString(16)
+			.replace('-', '0')
+
+	const debounce = (f, timeout = 300) => {
+		let timer
+		return (...args) => {
+			clearTimeout(timer)
+			timer = setTimeout(() => {
+				f.apply(this, args)
+			}, timeout)
+		}
 	}
-}
 
-const findNames = debounce(() => {
-	const names = Array.from(
-		document.querySelectorAll('.participants-item__display-name'),
-	).map((name) => name.textContent)
-	console.log(names)
-	chrome.runtime.sendMessage({ participantsCount: names.length })
-})
+	let lastKnownParticipants = []
 
-const observer = new MutationObserver((mutationsList) => {
-	mutationsList.forEach((mutation) => {
-		mutation.addedNodes.forEach(() => {
-			findNames()
+	const findParticipants = debounce(() => {
+		const participants = Array.from(
+			document.querySelectorAll('.participants-item-position'),
+		).map((participant) => {
+			const name = participant.querySelector(
+				'.participants-item__display-name',
+			).textContent
+			const avatar = participant.querySelector('.participants-item__avatar')
+			const id = (() => {
+				let base = name
+				if (avatar.tagName === 'IMG') {
+					base += avatar.src
+				} else {
+					base += avatar.getAttribute('style') + avatar.textContent
+				}
+				return hash(base)
+			})()
+
+			return {
+				id,
+				name,
+			}
+		})
+		const addParticipants = participants.filter(({ id }) =>
+			lastKnownParticipants.every(({ id: id2 }) => id === id2),
+		)
+		const removeParticipants = lastKnownParticipants.filter(({ id }) =>
+			participants.every(({ id: id2 }) => id === id2),
+		)
+
+		lastKnownParticipants = participants
+		chrome.runtime.sendMessage({
+			addParticipants,
+			removeParticipants,
+			participantsCount: participants.length,
 		})
 	})
-})
 
-// @TODO: maybe use setInterval/Timeout too
-observer.observe(document, { subtree: true, childList: true })
+	const loop = () => {
+		findParticipants()
+		setTimeout(loop, 1500)
+	}
+
+	chrome.runtime.sendMessage({ reset: true })
+	setTimeout(loop, 3000)
+})()
